bd18ab24e097f320b33c605fb5762b45f8104f1f
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-08-28 15:31:25 +0800

    setting up base

---
+++ index.js
@@ -0,0 +1,23 @@
+//===== require all dependencies
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//== add all middlewares
+require("./config/db"); // calls mongoose connection to clean this file
+app.use(express.json()); // allows me to receive JSON files from HEADER of REQUEST
+app.use(cors()); // allows all requests from outside servers or apps
+
+//== setup my routes
+// app.use("/api/...", require("./routes/.route"));
+
+//== 404 errors
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "404 not found", code: "EB404" });
+});
+
+//== setup the server port
+app.listen(process.env.PORT, () =>
+  console.log(`running on ${process.env.PORT}`)
+);
