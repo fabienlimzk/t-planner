f5671fce113bfb7d4ebd2f51ed4ca90efed18503
Randalla <the.randall.lim@gmail.com>
2020-08-29 18:34:20 +0800

    activities touch up paused, to rebase master

--- frontend/src/component/activities/AddActivity.jsx
+++ frontend/src/component/activities/AddActivity.jsx
@@ -4,41 +4,41 @@ import Axios from "axios";
 const URL = process.env.REACT_APP_URL;
 
 export default class AddActivity extends Component {
-    state = {
-        description: "",
-    };
+  state = {
+    description: "",
+  };
 
-    changeHandler = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    submitHandler = () => {
-        console.log(this.state);
-        Axios.post(`${URL}/activities`, this.state)
-            .then((res) => {
-                console.log("done");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+  submitHandler = () => {
+    console.log(this.state);
+    Axios.post(`${URL}/activities`, this.state)
+      .then((res) => {
+        console.log("done");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    render() {
-        let { description } = this.state;
-        return (
-            <div>
-                <h1>Add Activity</h1>
-                <div>
-                    <Row>
-                        <Form.Control
-                            name="description"
-                            value={description}
-                            onChange={this.changeHandler}
-                        />
-                    </Row>
-                    <Button onClick={this.submitHandler}>Submit</Button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    let { description } = this.state;
+    return (
+      <div>
+        <h1>Add Activity</h1>
+        <div>
+          <Row>
+            <Form.Control
+              name="description"
+              value={description}
+              onChange={this.changeHandler}
+            />
+          </Row>
+          <Button onClick={this.submitHandler}>Submit</Button>
+        </div>
+      </div>
+    );
+  }
 }
