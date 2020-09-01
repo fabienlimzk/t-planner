d234e3c70a1cc23522d467e01e39abf1be0ad474
Randalla <the.randall.lim@gmail.com>
2020-09-01 19:11:52 +0800

    geoloc pause

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -4,12 +4,29 @@ import { Container, Button, Row, Card, Col, Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import SimpleMap from "./map/SimpleMap";
 import SearchBox from "./map/SearchBox";
+import { Marker } from "react-google-maps";
+import { InfoWindow } from "react-google-maps";
+import Autocomplete from "react-google-autocomplete";
+import Map from "./map/Map";
 
 const URL = process.env.REACT_APP_URL;
 
 export default class Home extends Component {
   state = {
     trips: [],
+
+    // address: address ? address : "",
+    // area: area ? area : "",
+    // city: city ? city : "",
+    // state: state ? state : "",
+    // markerPosition: {
+    //   lat: latValue,
+    //   lng: lngValue,
+    // },
+    // mapPosition: {
+    //   lat: latValue,
+    //   lng: lngValue,
+    // },
   };
 
   fetchTrips = () => {
@@ -45,6 +62,14 @@ export default class Home extends Component {
   }
 
   render() {
+    // const address = place.formatted_address,
+    //   addressArray = place.address_components,
+    //   city = this.getCity(addressArray),
+    //   area = this.getArea(addressArray),
+    //   state = this.getState(addressArray),
+    //   latValue = place.geometry.location.lat(),
+    //   lngValue = place.geometry.location.lng();
+    // Set these values in the state.
     // console.log(this.props.trips);
     return (
       <div>
@@ -84,11 +109,54 @@ export default class Home extends Component {
               </Col>
             ))}
           </Row>
-          <SearchBox/>
-          {/* <SearchBox
-         placeholder={"123 anywhere st."}
-         onPlacesChanged={this.handleSearch} /> */}
+
+          <SearchBox />
+
           <SimpleMap />
+
+          <Marker
+            google={this.props.google}
+            name={"Dolores park"}
+            draggable={true}
+            onDragEnd={this.onMarkerDragEnd}
+            position={{
+              lat: this.state.markerPosition.lat,
+              lng: this.state.markerPosition.lng,
+            }}
+          />
+
+          <InfoWindow
+            onClose={this.onInfoWindowClose}
+            position={{
+              lat: this.state.markerPosition.lat + 0.0018,
+              lng: this.state.markerPosition.lng,
+            }}
+          >
+            <div>
+              <span style={{ padding: 0, margin: 0 }}>
+                {this.state.address}
+              </span>
+            </div>
+          </InfoWindow>
+
+          <Autocomplete
+            style={{
+              width: "100%",
+              height: "40px",
+              paddingLeft: "16px",
+              marginTop: "2px",
+              marginBottom: "100px",
+            }}
+            onPlaceSelected={this.onPlaceSelected}
+            types={["(regions)"]}
+          />
+
+          <Map
+            google={this.props.google}
+            center={{ lat: 18.5204, lng: 73.8567 }}
+            height="300px"
+            zoom={15}
+          />
         </Container>
         {/* ) : (
           "You cannot see this"
