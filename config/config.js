0076e694f60ae14a82b86fd2f6c313cd9d32f88b
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-09-01 17:34:59 +0800

    nothing much done

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, "this is a secret");
+    const decoded = jwt.verify(token, process.env.SECRET);
 
     req.user = decoded.user;
     next();
